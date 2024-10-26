fa55751186598fda8aa59430267a09be99d882f4
TjasaZil <tjasa.zilavec@gmail.com>
2024-10-26 13:57:41 +0200

    .

--- lib/widgets/expenses.dart
+++ lib/widgets/expenses.dart
@@ -1,5 +1,6 @@
 import 'package:finance_tracker_app/widgets/expenses_list.dart';
 import 'package:finance_tracker_app/models/expense.dart';
+import 'package:finance_tracker_app/widgets/new_expense.dart';
 import 'package:flutter/material.dart';
 
 class Expenses extends StatefulWidget {
@@ -30,14 +31,28 @@ class _ExpensesState extends State<Expenses> {
         category: Category.food),
   ];
 
+  void _openAddExpenseOverlay() {
+    showModalBottomSheet(
+      context: context,
+      builder: (ctx) => const NewExpense(),
+    );
+  }
+
   @override
   Widget build(context) {
     return Scaffold(
+        appBar: AppBar(
+          title: const Text('Flutter expense tracker'),
+          actions: [
+            IconButton(
+                onPressed: _openAddExpenseOverlay, icon: const Icon(Icons.add))
+          ],
+        ),
         body: Column(
-      children: [
-        const Text('chart'),
-        Expanded(child: ExpensesList(expenses: _registeredExpenses))
-      ],
-    ));
+          children: [
+            const Text('chart'),
+            Expanded(child: ExpensesList(expenses: _registeredExpenses))
+          ],
+        ));
   }
 }
